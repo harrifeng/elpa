f7233fdb73957c211f356dc873b79e61b420b8d4
Harri Feng <harrifeng@gmail.com>
2022-10-14 22:04:20 +0800

    add load-theme-buffer-local

---
+++ load-theme-buffer-local-20120702.2036/load-theme-buffer-local-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from load-theme-buffer-local.el  -*- no-byte-compile: t -*-
+(define-package "load-theme-buffer-local" "20120702.2036" "Install emacs24 color themes by buffer." 'nil :commit "e606dec66f16a06140b9aad625a4fd52bca4f936" :authors '(("Victor Borja" . "vic.borja@gmail.com")) :maintainer '("Victor Borja" . "vic.borja@gmail.com") :keywords '("faces") :url "http://github.com/vic/color-theme-buffer-local")
