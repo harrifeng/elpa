b41b31e6753b38587d0d6faef497200af164f032
Haoran Feng <harrifeng@gmail.com>
2023-09-25 11:45:04 +0000

    port to 29.1

---
+++ helm-20230924.524/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20230924.524" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.4")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "53e158c158efe1cb91b9359ebd6ed5cf99aee300" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ lsp-metals-20230817.827/lsp-metals-pkg.el
@@ -0,0 +1,24 @@
+(define-package "lsp-metals" "20230817.827" "Scala Client settings"
+  '((emacs "27.1")
+    (scala-mode "1.1")
+    (lsp-mode "7.0")
+    (lsp-treemacs "0.2")
+    (dap-mode "0.3")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.0")
+    (treemacs "2.5")
+    (posframe "1.4.1"))
+  :commit "811f02e4416d208102350ea48404e0310ffea647" :authors
+  '(("Ross A. Baker" . "ross@rossabaker.com")
+    ("Evgeny Kurnevsky" . "kurnevsky@gmail.com"))
+  :maintainers
+  '(("Ross A. Baker" . "ross@rossabaker.com"))
+  :maintainer
+  '("Ross A. Baker" . "ross@rossabaker.com")
+  :keywords
+  '("languages" "extensions")
+  :url "https://github.com/emacs-lsp/lsp-metals")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
