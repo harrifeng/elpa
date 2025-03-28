ae9b50903be18afdfd2936051b279627f4a5a008
Haoran Feng <haoran.feng@transsion.com>
2025-03-28 17:42:24 +0800

    Upgrade to emacs 30 and update elpa

---
+++ flycheck-golangci-lint-20250211.1910/flycheck-golangci-lint-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "flycheck-golangci-lint" "20250211.1910"
+  "Flycheck checker for golangci-lint."
+  '((emacs    "24")
+    (flycheck "0.22"))
+  :url "https://github.com/weijiangan/flycheck-golangci-lint"
+  :commit "424ba1b3a13f5548c440b7a25822932ad4b51cd6"
+  :revdesc "424ba1b3a13f"
+  :keywords '("convenience" "tools" "go")
+  :authors '(("Wei Jian Gan" . "weijiangan@outlook.com"))
+  :maintainers '(("Wei Jian Gan" . "weijiangan@outlook.com")))
