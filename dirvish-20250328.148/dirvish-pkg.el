b2c5941d70e5e5f18295847dfe3a2babba228f50
Haoran Feng <haoran.feng@transsion.com>
2025-04-03 15:18:16 +0800

    new dirvish

---
+++ dirvish-20250328.148/dirvish-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dirvish" "20250328.148"
+  "A modern file manager based on dired mode."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/alexluigit/dirvish"
+  :commit "2d63995d3296c259cef90830cf0433602429ecec"
+  :revdesc "2d63995d3296"
+  :keywords '("files" "convenience")
+  :authors '(("Alex Lu" . "https://github.com/alexluigit"))
+  :maintainers '(("Alex Lu" . "https://github.com/alexluigit")))
