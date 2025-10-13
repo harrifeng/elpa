78106e9a5e05b62484fe620b378c523ed3d064ac
Haoran Feng <haoran.feng@transsion.com>
2025-10-13 10:26:09 +0800

    new update packages

---
+++ flycheck-golangci-lint-20250910.1938/flycheck-golangci-lint-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "flycheck-golangci-lint" "20250910.1938"
+  "Flycheck checker for golangci-lint."
+  '((emacs    "24")
+    (flycheck "0.22"))
+  :url "https://github.com/weijiangan/flycheck-golangci-lint"
+  :commit "38cc30eb8b3056260993bd085f5ae6bc90af177f"
+  :revdesc "38cc30eb8b30"
+  :keywords '("convenience" "tools" "go")
+  :authors '(("Wei Jian Gan" . "weijiangan@outlook.com"))
+  :maintainers '(("Wei Jian Gan" . "weijiangan@outlook.com")))
