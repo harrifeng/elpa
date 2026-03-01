c736935d91d777d3f99993d44c97c94e39751287
Haoran Feng <haoran.feng@transsion.com>
2026-03-01 20:29:39 +0800

    new package installed for eglot instead of lsp

---
+++ spacemacs-theme-20251221.1656/spacemacs-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "spacemacs-theme" "20251221.1656"
+  "Color theme with a dark and light versions."
+  ()
+  :url "https://github.com/nashamri/spacemacs-theme"
+  :commit "5635d6bbc76e6f06b99fa5dac6e6fd6675459ca6"
+  :revdesc "5635d6bbc76e"
+  :keywords '("color" "theme"))
